067edb1fc490a8856b25c5d6bf37247104018af8
lungen <lungenfetzer@gmail.com>
2017-10-30 11:57:04 +0100

    ok, fine again

---
+++ mysite/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse, Http404
+from django.template.loader import get_template
+import datetime
+
+
+def hello(request):
+    # request is an instance of the class django.http.HttpRequest.
+    return HttpResponse("Hello world")
+
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    t = get_template('current_datetime.html')
+    html = t.render({'current_date': now})
+    return HttpResponse(html)
+
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    assert False
+    html = "<html><body>In %s hour(s), it will be  %s.</body></html>" % (offset, dt) # noqa
+    return HttpResponse(html)
