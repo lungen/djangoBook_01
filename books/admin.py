d07b9bbedcea6589f2bcf78fb08c654452ca5cf5
lungen <lungenfetzer@gmail.com>
2017-11-01 00:49:23 +0100

    ok

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Publisher, Author, Book
 
-# Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name')
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publisher', 'publication_date')
+    list_filter = ('publication_date',)
+
+
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'country', 'website')
+
+
+admin.site.register(Publisher, PublisherAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
